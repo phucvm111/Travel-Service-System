35483649d18c40beda20618ba8c78823ae46df28
phucvm111 <phucthcsvt@gmail.com>
2026-03-27 15:20:17 +0700

    add forger password

---
+++ TravelSystem/Pages/Auths/ForgotPassword.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Mail;
+using TravelSystem.Models;
+
+namespace TravelSystem.Pages.Auths
+{
+    public class ForgotPasswordModel : PageModel
+    {
+        private readonly FinalPrnContext _context;
+
+        public ForgotPasswordModel(FinalPrnContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        public string Error { get; set; }
+
+        public IActionResult OnPost()
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Gmail == Email);
+
+            if (user == null)
+            {
+                Error = "Email không tồn tại!";
+                return Page();
+            }
+
+            var otp = new Random().Next(100000, 999999).ToString();
+
+            HttpContext.Session.SetString("OTP_HASH", BCrypt.Net.BCrypt.HashPassword(otp));
+            HttpContext.Session.SetString("OTP_EXPIRE", DateTime.Now.AddMinutes(2).ToString());
+            HttpContext.Session.SetString("RESET_EMAIL", Email);
+
+            SendEmail(Email, otp);
+
+            return RedirectToPage("VerifyOTP");
+        }
+
+        private void SendEmail(string toEmail, string otp)
+        {
+            var fromEmail = "phucthcsvt@gmail.com";
+            var password = "tuqd lxyo batr xftl";
+
+            MailMessage mail = new MailMessage(fromEmail, toEmail);
+            mail.Subject = "OTP Reset Password";
+            mail.Body = $"OTP: {otp} (hết hạn 2 phút)";
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential(fromEmail, password);
+            smtp.EnableSsl = true;
+
+            smtp.Send(mail);
+        }
+    }
+}
---
+++ TravelSystem/Pages/Auths/ResetPassword.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
+using TravelSystem.Models;
+
+namespace TravelSystem.Pages.Auths
+{
+    public class ResetPasswordModel : PageModel
+    {
+        private readonly FinalPrnContext _context;
+
+        public ResetPasswordModel(FinalPrnContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public string NewPassword { get; set; }
+
+        public string Error { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (HttpContext.Session.GetString("OTP_VERIFIED") != "true")
+                return RedirectToPage("Login");
+
+            if (!Regex.IsMatch(NewPassword,
+                @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$"))
+            {
+                Error = "Mật khẩu >=8 ký tự, có chữ hoa, thường, số, ký tự đặc biệt!";
+                return Page();
+            }
+
+            var email = HttpContext.Session.GetString("RESET_EMAIL");
+            var user = _context.Users.FirstOrDefault(x => x.Gmail == email);
+
+            //user.Password = BCrypt.Net.BCrypt.HashPassword(NewPassword);
+            user.Password = NewPassword;
+
+            _context.SaveChanges();
+
+            HttpContext.Session.Clear();
+
+            return RedirectToPage("Login");
+        }
+    }
+}
